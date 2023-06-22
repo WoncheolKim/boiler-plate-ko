b49fbefbfd5f7d8f0eba2dc13d0110c2ebd1f845
Woncheol Kim <icijiy@gmail.com>
2023-06-22 16:43:04 -0700

    Created Router

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://WonKim:11223344@cluster0.qw1qyc6.mongodb.net/?retryWrites=true&w=majority', {
@@ -8,11 +14,20 @@ mongoose.connect('mongodb+srv://WonKim:11223344@cluster0.qw1qyc6.mongodb.net/?re
 }).then(() => console.log('MongoDB connected..'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
