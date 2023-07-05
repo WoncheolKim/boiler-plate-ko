b19210d8f63671ff302bfe1145bc070362dc684f
Woncheol Kim <icijiy@gmail.com>
2023-07-05 14:31:33 -0700

    Generated token with jsonwebtoken

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require("./models/User");
@@ -10,6 +10,7 @@ const { User } = require("./models/User");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -49,11 +50,12 @@ app.post('/login', (req, res) => {
         user.generateToken((err, user) => {
           if (err) return res.status(400).send(err);
 
+          res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
         })
     })
-
   })
-
 })
 
 app.listen(port, () => {
